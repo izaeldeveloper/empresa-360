1444879697ec78b42d24c1dff8db894aaea1f261
Izael <izael.developer@poton.me>
2025-04-10 07:01:08 -0300

    Componente Leads e Primeira Chamada de API Com JSON Server

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-
+  <!--Implementa a Visualização da Rota Atual-->
   <router-view />
 </template>
 
 <script>
-
+//Export default comum do Componente.
 export default {
   name: 'App',
 }
